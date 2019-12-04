77b707d547674fb97d9f2b936cc06ce6a97d35bd
LilBudj <kinich2001@gmail.com>
2019-12-04 18:23:13 +0300

    stylizing refactoring [3]

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,13 +14,13 @@ const Nav = (props) => {
     return(
         <div className={style.nav}>
             <ul className={style.navList}>
-                <li><NavLink to='/Profile' activeClassName={style.aitem} className={style.item}> Profile</NavLink></li>
-                <li><NavLink to='/Feed' activeClassName={style.aitem} className={style.item}>Feed</NavLink></li>
-                <li><NavLink to='/Dialogs' activeClassName={style.aitem} className={style.item}>Messages</NavLink></li>
-                <li><NavLink to='/Music' activeClassName={style.aitem} className={style.item}>Music</NavLink></li>
-                <li><NavLink to='/Settings' activeClassName={style.aitem} className={style.item}>Settings</NavLink></li>
-                <li><NavLink to='/Games' activeClassName={style.aitem} className={style.item}>Games</NavLink></li>
-                <li><NavLink to='/Users' activeClassName={style.aitem} className={style.item}>Users</NavLink></li>
+                <li><NavLink to='/Profile' activeClassName={style.aitem} className={style.item}><button>Profile</button></NavLink></li>
+                <li><NavLink to='/Feed' activeClassName={style.aitem} className={style.item}><button>Feed</button></NavLink></li>
+                <li><NavLink to='/Dialogs' activeClassName={style.aitem} className={style.item}><button>Messages</button></NavLink></li>
+                <li><NavLink to='/Music' activeClassName={style.aitem} className={style.item}><button>Music</button></NavLink></li>
+                <li><NavLink to='/Settings' activeClassName={style.aitem} className={style.item}><button>Settings</button></NavLink></li>
+                <li><NavLink to='/Games' activeClassName={style.aitem} className={style.item}><button>Games</button></NavLink></li>
+                <li><NavLink to='/Users' activeClassName={style.aitem} className={style.item}><button>Users</button></NavLink></li>
             </ul>
         </div>
         );
